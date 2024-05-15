c58b484155684f30a5e43e14f33f4247b4b62f6f
RyunosukeFunasako <e205732@ie.u-ryukyu.ac.jp>
2024-05-15 17:06:50 +0900

    class-validatorを利用した入力値バリデーション

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,21 @@
+import { Type } from "class-transformer";
+import { IsInt, IsNotEmpty, IsString, MaxLength, Min } from "class-validator";
+
 export class CreateItemDto {
+    // 文字列かどうかのバリデーション
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(40)
     name: string;
+
+    @IsInt()
+    @Min(1)
+    // リクエストは文字列で渡ってくることがあるため、
+    // class-transformerのTypeデコレーターを使ってプロパティをnumber型だと認識されるようにする
+    @Type(() => Number)
     price: number;
+
+    @IsString()
+    @IsNotEmpty()
     description: string;
 }
